95f8c27243a5fdfef1feabfdd85b647cd847442b
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2025-04-11 11:04:55 -0700

    Finished

--- Week1/hello-react-native/components/GroceryList.js
+++ Week1/hello-react-native/components/GroceryList.js
@@ -1,9 +1,26 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Button,
+} from "react-native";
 import { useState } from "react";
 
 export default function GroceryList() {
   //make a groceryList and add to state
   const [groceries, setGroceries] = useState(["Milk", "Eggs", "Bread"]);
+  const [newGrocery, setNewGrocery] = useState("");
+  function groceryHandler() {
+    if (!newGrocery) {
+      alert("You must type in a grocery");
+      return;
+    }
+    const newArray = [...groceries, newGrocery];
+    setGroceries(newArray);
+    setNewGrocery("");
+  }
   //this is a componenet
   //item and index will be passed to this component via props
   function renderGrocery({ item, index }) {
@@ -29,6 +46,12 @@ export default function GroceryList() {
         {/* Take the last 10 minutes - Add an input and a button
         When button is clicked add item to the grocerlist. If input is empty
         Do not add item to grocerylist */}
+        <TextInput
+          value={newGrocery}
+          onChangeText={setNewGrocery}
+          placeholder="New Item"
+        />
+        <Button onPress={groceryHandler} title="Add Grocery" />
       </View>
     </View>
   );
