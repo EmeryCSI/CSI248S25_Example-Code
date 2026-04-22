9723abbc1b37b326a3d49274dd48bd871675dfd5
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-04-22 14:45:23 -0700

    Update RowContainer.js

--- Week3/styling/screens/RowContainer.js
+++ Week3/styling/screens/RowContainer.js
@@ -1,26 +1,13 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-//Create a horizontal row of 4 boxes where the first box takes up
-//half of the screen. The second box takes of half of the remaining space
-//The 3rd and 4th box split the remaining space evenly
 
-export default function ColumnContainer() {
+export default function RowContainer() {
   return (
     <View style={styles.container}>
-      <View style={[styles.box, styles.box1]}>
-        <Text style={styles.text}>1/2</Text>
-      </View>
-
-      <View style={[styles.box, styles.box2]}>
-        <Text style={styles.text}>1/4</Text>
-      </View>
-
-      <View style={[styles.box, styles.box3]}>
-        <Text style={styles.text}>1/8</Text>
-      </View>
-
-      <View style={[styles.box, styles.box4]}>
-        <Text style={styles.text}>1/8</Text>
+      <View style={styles.row}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
       </View>
     </View>
   );
@@ -29,31 +16,22 @@ export default function ColumnContainer() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row", // Arrange children horizontally
+    backgroundColor: "whiteSmoke",
   },
-  box: {
-    justifyContent: "center",
-    alignItems: "center",
+  row: {
+    flex: 1,
+    flexDirection: "row",
   },
   box1: {
-    flex: 4,
-    backgroundColor: "coral",
+    flex: 1,
+    backgroundColor: "#7f1c1c",
   },
   box2: {
     flex: 2,
-    backgroundColor: "turquoise",
+    backgroundColor: "#22a032",
   },
   box3: {
-    flex: 1,
-    backgroundColor: "rosybrown",
-  },
-  box4: {
-    flex: 1,
-    backgroundColor: "seagreen",
-  },
-  text: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
+    flex: 3,
+    backgroundColor: "#3f1c7f",
   },
 });
